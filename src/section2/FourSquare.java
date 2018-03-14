d3ec2fb05e3a79fc92dbf8f07edf3213c0992cea
Quimbap <League@AdministorsiMac.attlocal.net>
2018-03-13 19:56:48 -0700

    Day 2

--- src/section2/FourSquare.java
+++ src/section2/FourSquare.java
@@ -4,30 +4,40 @@ import javax.swing.JOptionPane;
 import org.jointheleague.graphical.robot.Robot;
 
 public class FourSquare {
-	
+
 	// 2. Create a new Robot
+	Robot dude = new Robot();
 
 	void go() {
 		// 4. Make the robot move as fast as possible
-
+		dude.setSpeed(100);
 		// 5. Set the pen width to 5
 
 		// 6. Use a for loop to repeat steps #7 to #8, four times...
-
+		for (int i = 0; i < 4; i++) {
 			// 7. Set the pen color to random
-	
+			dude.setRandomPenColor();
+			dude.penDown();
 			// 1. Call the drawSquare() method
-	
+			drawSquare();
 			// 8. Turn the robot 90 degrees to the right
-
+			dude.turn(90);
+		}
 	}
 
 	/* 3. Fill in the code to draw a square inside the method below. */
 	void drawSquare() {
-		JOptionPane.showMessageDialog(null, "yay! you called the drawSquare() method!");
-
-		
-		
+		// JOptionPane.showMessageDialog(null, "yay! you called the drawSquare()
+		// method!");
+		for (int i = 0; i < 5; i++) {
+			dude.move(90);
+			dude.turn(45);
+			dude.sparkle();
+			dude.turn(90);
+			dude.move(34);
+			dude.turn(45);
+			dude.move(97);
+		}
 	}
 
 	public static void main(String[] args) {
@@ -35,6 +45,3 @@ public class FourSquare {
 	}
 
 }
-
-
-
--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -4,12 +4,19 @@ import java.awt.Color;
 import org.jointheleague.graphical.robot.Robot;
 
 public class MyFirstJavaProgram {
-	
+
 	public static void main(String[] args) {
-		
+
 		// START HERE
-	
-		
-		
+		Robot dude = new Robot();
+		dude.setPenColor(Color.CYAN);
+		dude.sparkle();
+		dude.setSpeed(100);
+		dude.penDown();
+		for (int i = 0; i < 4; i++) {
+			dude.move(70);
+			dude.turn(90);
+
+		}
 	}
 }
